8369080b7e89f6f1acd1aa7b6bf2f14b71c61643
ebencomoba <ebencomoba@miners.utep.edu>
2021-02-22 00:23:20 -0700

    Implemented memory allocation functions

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 
 /* Return true (non-zero) if c is a whitespace characer
@@ -22,13 +23,12 @@ int non_space_char(char c)
    str does not contain any words. */
 char *word_start(char *str)
 {
+  /* If we see a non_space char, we found the start of the word */
   while (*str) {
-    if (space_char(*str)) {
-      str++;
-    }
-    else {
+    if (non_space_char(*str)) {
       return str;
     }
+    str++;
   }
   return '\0';
 }
@@ -36,13 +36,12 @@ char *word_start(char *str)
 /* Returns a pointer terminator char following *word */
 char *word_terminator(char *word)
 {
+  /* If we see a space char, we found the end of the word */
   while (*str) {
-    if (non_space_char(*str)) {
-      str++;
-    }
-    else {
+    if (space_char(*str)) {
       return str;
     }
+    str++;
   }
   return '\0';
 }
@@ -50,6 +49,8 @@ char *word_terminator(char *word)
 /* Counts the number of words in the string argument. */
 int count_words(char *str)
 {
+  /* We find the number of words with word_start
+     and end them with word terminator */
   int numWords = 0;
   while (*str) {
     str = word_start(str);
@@ -61,7 +62,16 @@ int count_words(char *str)
 
 /* Returns a fresly allocated new zero-terminated string 
    containing <len> chars from <inStr> */
-char *copy_str(char *inStr, short len);
+char *copy_str(char *inStr, short len)
+{
+  /* We make space for the string and copy the chars to it */
+  char *token_ptr = malloc((len+1) * sizeof(char));
+  for (int counter = 0; counter < len; counter) {
+    *(token_ptr + counter) = *(inStr + counter);
+  }
+  *(token_ptr + len) = '\0';
+  return token_ptr;
+}
 
 /* Returns a freshly allocated zero-terminated vector of freshly allocated 
    space-separated tokens from zero-terminated str.
@@ -75,9 +85,24 @@ char *copy_str(char *inStr, short len);
 char **tokenize(char* str);
 
 /* Prints all tokens. */
-void print_tokens(char **tokens);
+void print_tokens(char **tokens)
+{
+  /* We print all tokens */
+  int counter = 0;
+  while (**tokens) {
+    printf("tokens[%d] = \"%s\"\n", counter, *tokens);
+    counter++;
+    tokens++;
+  }
+}
 
 /* Frees all tokens and the vector containing themx. */
-void free_tokens(char **tokens);
-
-#endif
+void free_tokens(char **tokens)
+{
+  /* We free the memory from the tokens and the token vector */
+  while (**tokens) {
+    free(*tokens);
+    tokens++;
+  }
+  free(tokens);
+}
