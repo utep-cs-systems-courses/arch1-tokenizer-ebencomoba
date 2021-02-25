fc6cca31c8f187b0aa609b7dc7d2c1db3ab219d9
ebencomoba <ebencomoba@miners.utep.edu>
2021-02-25 09:56:43 -0700

    Added tokenize function

--- src/uimain.c
+++ src/uimain.c
@@ -1,20 +1,35 @@
-#include <stdio.h>                //Working with strings
+#include <stdio.h>      //Working with strings
+#include <stdlib.h>
+#include "tokenizer.h"
 
 int main () {
   puts("Welcome");
   puts("I will be a temporary UI while we work on this project");
-  puts("Meanwhile, I will repeat what you write");
 
   while(1) {       /*Infinite loop */
-    char sentence[70];
     printf("\nWrite your sentence or enter q to quit:\n> ");
-    fgets(sentence, 70, stdin);
-    if (sentence[0] == 'q' && sentence[1] == '\n') {
+    /* We obtaing a sentence of max length 100,
+       leaving space to zero-terminate it */
+    char sentence[100];
+    char *ptr = sentence;
+    int counter = 0;
+    char c;
+    while ((c = getchar()) != '\n' && counter < 99) {
+      *(ptr + counter) = c;
+      counter++;
+    }
+    *(ptr + counter) = '\0';
+    /* If the user quits, we exit the loop */
+    if (sentence[0] == 'q' && sentence[1] == '\0') {
       puts("\nThank you!");
       break;
     }
-    else
-      printf("You wrote: %s", sentence);
+    else {
+      printf("You wrote: %s\n", sentence);
+      char **tokenVector = tokenize(ptr);
+      print_tokens(tokenVector);
+      free_tokens(tokenVector);
+    }
   }
   return 0;
 }
